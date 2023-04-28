632271745fa9b1b84ad718512c051f62f70f0a76
Sherlock <1075773551@qq.com>
2023-04-28 12:49:09 +0800

    hahaha

--- icephoneTest/app/src/main/java/com/example/icephonetest/PublicResult.java
+++ icephoneTest/app/src/main/java/com/example/icephonetest/PublicResult.java
@@ -1,4 +1,20 @@
 package com.example.icephonetest;
 
-public class PublicResult {
+import java.io.Serializable;
+
+public class PublicResult implements Serializable {
+    Integer id;
+    String title;
+    String content;
+    String statues;
+    String date;
+    String postScript;
+    String kind;
+
+    public PublicResult(String title, String content, String date,String kind) {
+        this.title = title;
+        this.content = content;
+        this.date = date;
+        this.kind = kind;
+    }
 }
